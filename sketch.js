306eda7e7bd93f67f9ca5b4660589b0bee236871
i❤computers <tomelovispruce@ilovecomputers.org>
2018-06-14 01:31:08 -0700

    See what multiple instances of BinarySpheres look like

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-let angle = 0;
-
+let binary1, binary2, binary3;
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
+	binary1 = new BinarySphere();
+	binary2 = new BinarySphere();
+	binary3 = new BinarySphere();
 }
 
 function windowResized() {
@@ -10,19 +12,18 @@ function windowResized() {
 
 function draw() {
 	background(175);
+	binary1.rotate();
+	binary1.draw();
 
-	fill(0);
-	rotateY(angle);
-
+	binary2.rotate();
 	push();
-	translate(100, 0);
-	sphere(100);
+	translate(0, 150);
+	binary2.draw();
 	pop();
 
+	binary3.rotate();
 	push();
-	translate(-100, 0);
-	sphere(100);
+	translate(0, -150);
+	binary3.draw();
 	pop();
-
-	angle += 0.07;
 }
