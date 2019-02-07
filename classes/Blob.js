540fdbc4ccb8fd35840d70ba7ebfc99f62f6d1f5
i❤computers <tomelovispruce@ilovecomputers.org>
2019-02-06 22:02:07 -0800

    Create Blob Class

---
+++ classes/Blob.js
@@ -0,0 +1,37 @@
+class Blob {
+	constructor() {
+		this.radianStep = 1;
+		this._noiseCylinderSliceZ = 0;
+	}
+
+	draw() {
+		push();
+
+		let radius = 150;
+
+		// This is done so the input to noise are:
+		//     * non-zero
+		//     * are unique pairs
+		//     * go in circle so they smoothly return
+		//       to their original point
+		// Learned this technique from this sketch: http://p5ide.herokuapp.com/editor#?sketch=57d45de23b7b6903001548c4﻿
+		const noiseCircleCenterOffset = 1;
+
+		stroke(255);
+		noFill();
+		beginShape();
+		for (let pointAlongCircle = 0; pointAlongCircle < TWO_PI; pointAlongCircle += radians(this.radianStep)) {
+			let noiseCircleX = noiseCircleCenterOffset + cos(pointAlongCircle);
+			let noiseCircleY = noiseCircleCenterOffset + sin(pointAlongCircle);
+			let offset = map(noise(noiseCircleX, noiseCircleY, this._noiseCylinderSliceZ), 0, this.radianStep, -25, 25);
+			let r = radius + offset;
+			let x = r * cos(pointAlongCircle);
+			let y = r * sin(pointAlongCircle);
+			vertex(x, y);
+		}
+		endShape(CLOSE);
+
+		this._noiseCylinderSliceZ += 0.01;
+		pop();
+	}
+}
